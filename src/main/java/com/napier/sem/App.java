d923320fcb6589e1537110d318d604f48bd96759
Shaka S. Dow <shakad@yahoo.com>
2024-03-04 19:43:47 -0400

    Added additional comments

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,54 +4,68 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 
+
+/**
+ * Class containing the main static method.
+ */
 public class App
 {
-
-    /**
-     * Connection to MySQL database.
-     */
+    // Static variable that will reference connection to database once established
     public static Connection con = null;
 
-    /**
-     * Variables used to establish a connection to MySQL database.
-     */
+    // Host where world database is located
     private static final String DB_HOST = "db";  // Change to db when deploying using docker compose
+
+    // Port to connect to MySQL database
     private static final int DB_PORT = 3306;
+
+    // Login for MySQL database
     private static final String DB_LOGIN = "root";
+
+    // Password for MySQL database
     private static final String DB_PASSWORD = "group-7-gc";
+
+    // Number of times application will attempt to connect to MySQL (will wait 30 seconds between attempts)
     private static final int DB_CONNECTION_ATTEMPTS = 10;
+
 
     /**
      * Application execution starts here.
+     * In interactive mode, no arguments should be provided when executing the application.
+     * In non-interactive mode, the first argument should be the path of the XML report definition file.
+     *
      * @param args Command line arguments
      */
     public static void main(String[] args)
     {
-        // Connect to database
+        // Attempt to connect to the database
         App.connect();
         // Was connection successful?
         if (App.con != null) {
-            // Connection was successfully established!
-            // If an argument was supplieg (args[1]) then execute the report else be interactive
+            // Connection was successfully established!. If an argument was supplied (args[1]) then execute the report
             if (args.length > 0) {
-                // Run report
+                // Enter non-interactive mode by executing the report in args[0]
                 try {
                     new ReportHandler(args[0]);
                 } catch (IOException e) {
-                    System.out.println("Error: File not found at path " + args[1]);
+                    System.out.println("Error: File not found at path " + args[0]);
                 }
             } else {
-                // Display top menu (defined in index.xml file)
+                // Enter interactive mode by displaying top menu (defined in index.xml file)
                 try {
                     // Get URL for the index.xml file in the resources directory
                     URL indexUrl = App.class.getClassLoader().getResource("xml_config/index.xml");
+                    // Was there a problem?
                     if (indexUrl == null) {
+                        // Display error message then exit
                         System.out.println("Error: index.xml file was not found in the resources/xml_config directory");
-                        System.exit(1);
+                        System.exit(-1);
                     } else {
+                        // If there was no problem, then display the menu to the user
                         new MenuHandler(indexUrl.toString());
                     }
                 } catch (IOException e) {
+                    // Display IOException error message
                     System.out.println("Error: IOException encountered while processing index.xml file");
                 }
             }
@@ -64,21 +78,23 @@ public class App
 
 
     /**
-     * Connect to the MySQL database.
+     * Static method that attempts to connect to the MySQL database.
      */
     public static void connect()
     {
+        // Attempt to load the MySQL JDBC driver
         try
         {
-            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
-            System.out.println("Could not load SQL driver");
+            // Display error message and exit if unable to load the JDBC driver
+            System.out.println("Error: Could not load SQL driver");
             System.exit(-1);
         }
 
+        // Repeatedly attempt to connect to the MySQL database (wait 30 seconds between attempts)
         for (int i = 0; i < DB_CONNECTION_ATTEMPTS; ++i)
         {
             System.out.printf("Attempting to connect to database (attempt %d of %d)%n", i+1, DB_CONNECTION_ATTEMPTS);
@@ -94,21 +110,22 @@ public class App
             {
                 // Made less verbose by not displaying exception
             }
-            // Wait a bit before trying again to connect
+            // Sleep for 30 seconds before trying again to connect
             try {
                 Thread.sleep(30000);
             } catch (InterruptedException e) {
-                System.out.println("Thread interrupted? Should not happen.");
+                System.out.println("Error: Thread interrupted? Should not happen.");
             }
         }
     }
 
 
     /**
-     * Disconnect from the MySQL database.
+     * Static method that gracefully disconnects from the MySQL database.
      */
     public static void disconnect()
     {
+        // Close MySQL connection if one was established
         if (con != null)
         {
             try
@@ -118,9 +135,8 @@ public class App
             }
             catch (Exception e)
             {
-                System.out.println("Error closing connection to database");
+                System.out.println("Error: Error encountered while closing connection to database");
             }
         }
     }
-
 }
--- src/main/java/com/napier/sem/ReportParameter.java
+++ src/main/java/com/napier/sem/ReportParameter.java
@@ -2,29 +2,57 @@ package com.napier.sem;
 
 import java.util.Scanner;
 
+
+/**
+ * Class that holds the parameters obtained via user input.
+ */
 public class ReportParameter {
+    // Variable that will hold the name of the parameter requested
     private final String name;
+    // Variable that will hold the prompt that should be displayed to the user when requesting the parameter
     private final String prompt;
+    // Variable that will hold the parameter after it is obtained from the user
     private String value;
 
+
+    /**
+     * Class constructor.
+     *
+     * @param name Name of the parameter
+     * @param prompt Prompt that should be displayed to the user when the parameter is requested
+     */
     public ReportParameter(String name, String prompt){
         this.name = name;
         this.prompt = prompt;
     }
 
+
     /**
-     * Set value by asking user for it
+     * Method that will be called when requesting the parameter value from the user.
      */
     public void request(){
+        // Display the prompt
         System.out.printf("%s: ", this.prompt);
+        // Wait for the user to enter a value
         Scanner scanner = new Scanner(System.in);
         this.value = scanner.nextLine();
     }
 
+
+    /**
+     * Getter method for the name variable.
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * Getter method for the value of the parameter.
+     *
+     * @return value
+     */
     public String getValue() {
         return value;
     }
