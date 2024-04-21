c414f984adb5b713eea21262fcf058c4a43596e4
Shaka S. Dow <shakad@yahoo.com>
2024-04-21 18:48:45 -0400

    Integration tests

--- src/test/java/com/napier/sem/IntegrationTests.java
+++ src/test/java/com/napier/sem/IntegrationTests.java
@@ -5,12 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class IntegrationTests {
 
-    static App app;
-
     @BeforeAll
     static void init()
     {
-//        app = new App();
     }
 
     @Test
